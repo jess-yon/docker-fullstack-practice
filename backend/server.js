3890cb3e3244a21cfb670a5c5b25a2e763fc3a95
Yeonju Lee <jessy2yon@gmail.com>
2021-07-02 02:12:48 +0900

    created apis

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,29 @@ const port = 4000;
 
 app.use(express.json());
 
+// db에서 모든 정보 가져오기 => client에 전달
+app.get("/api/values", (req, res) => {
+  db.pool.query("SELECT * FROM lists;", (err, results, fields) => {
+    if (err) {
+      return res.status(500).send(err);
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+// client에서 입력한 값을 db에 전달하기
+app.post("/api/value", (req, res) => {
+  db.pool.query(`INSERT INTO lists (value) VALUES("${req.body.value}")`),
+    (err, results, fields) => {
+      if (err) {
+        return res.status(500).send(err);
+      } else {
+        res.json({ success: true, value: req.body.value });
+      }
+    };
+});
+
 app.listen(port, () => {
   console.log(`Server is starting on ${port}`);
 });
