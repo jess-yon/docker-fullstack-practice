62939dede5f24f67583fc82e8a7ff429bb51d413
Yeonju Lee <jessy2yon@gmail.com>
2021-07-02 02:33:59 +0900

    get list values using useEffect

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,19 @@
+import React, { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [lists, setLists] = useState([]);
+  const [value, setValue] = useState("");
+
+  useEffect(() => {
+    axios.get("/api/values").then((response) => {
+      console.log(response);
+      setLists(response.data);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
