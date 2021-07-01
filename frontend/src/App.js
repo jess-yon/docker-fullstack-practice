ff2015262d56ea3b520e9a8949010ae48ef7edbd
Yeonju Lee <jessy2yon@gmail.com>
2021-07-02 02:41:52 +0900

    onChange & onSubmit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,23 @@ function App() {
   const [lists, setLists] = useState([]);
   const [value, setValue] = useState("");
 
+  const changeHandler = (event) => {
+    setValue(event.currentTarget.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    axios.post("/api/value", { value: value }).then((response) => {
+      if (response.data.success) {
+        console.log("response: ", response);
+        setLists([...lists, response.data]);
+        setValue("");
+      } else {
+        alert("Request failed");
+      }
+    });
+  };
+
   useEffect(() => {
     axios.get("/api/values").then((response) => {
       console.log(response);
@@ -19,8 +36,14 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <div className="container">
-          <input type="text" placeholder="Please input text" />
-          <button type="submit">Submit</button>
+          <input
+            type="text"
+            placeholder="Please input text"
+            onChange={changeHandler}
+          />
+          <button type="submit" onSubmit={submitHandler}>
+            Submit
+          </button>
         </div>
       </header>
     </div>
